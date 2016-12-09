bd8d220286d6f801e6e2d6f4b41ab37b95552a27
mfito <mfito.13@gmail.com>
2016-12-09 07:55:55 +0000

    Add User model (including secure passwords)

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+    
+    # DB保存前にメールアドレスを小文字に
+    before_save { email.downcase! }
+    
+    # nameのバリデーション
+    validates :name,  presence: true, length: { maximum: 50 }
+    
+    # emailのバリデーション
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }   # 大文字小文字を無視して一意
+    
+    # passwordのバリデーション
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
+    
+end
