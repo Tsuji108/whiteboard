e6ca7e17750e0fb507a6ef66b71fa2047caaa6fa
mfito <mfito.13@gmail.com>
2016-12-10 07:10:36 +0000

    Finish signup part

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,6 +40,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # TODO:本番環境ではSSLを使う(今はとりあえずコメントアウト（デフォルトの設定）)
   # config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   
-<<<<<<< HEAD
-=======
   root 'users#new'
   
->>>>>>> master
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   resources :users
