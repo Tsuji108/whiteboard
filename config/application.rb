aac1a7cc398665e5dd79d553122657f25c660ece
mfito <mfito.13@gmail.com>
2016-12-13 06:32:41 +0000

    heroku test

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ Bundler.require(*Rails.groups)
 module Whiteboard
   class Application < Rails::Application
     config.i18n.default_locale = :ja
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
