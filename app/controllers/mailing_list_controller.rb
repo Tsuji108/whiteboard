b918514bef13e790e9c2f219f3b014ccd448508a
mfito <mfito.13@gmail.com>
2016-12-14 07:50:23 +0000

    Make MailingListController

---
+++ app/controllers/mailing_list_controller.rb
@@ -0,0 +1,4 @@
+class MailingListController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
+  get 'mailing_list/new'
+
   # TODO: 仮のルート
   root   'sessions#new'
 
