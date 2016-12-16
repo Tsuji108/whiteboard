9d022405b00695b23d98fe4f767f1c344a0c8968
mfito <mfito.13@gmail.com>
2016-12-16 22:10:02 +0000

    Add add_template_helper to UserMailer

--- app/helpers/mailing_lists_helper.rb
+++ app/helpers/mailing_lists_helper.rb
@@ -1,6 +1,10 @@
 module MailingListsHelper
   # メール作成時の複数行の改行表示に対応
+<<<<<<< HEAD
   def apply_multiple_br(target)
+=======
+  def br(target)
+>>>>>>> 4446601... Add add_template_helper to UserMailer
     target = html_escape(target)
     target.gsub(/\r\n|\r|\n/, "<br>")
   end
