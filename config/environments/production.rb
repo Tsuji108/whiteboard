32d49caf595e103441be8178f65c2c95a8fe2b9e
m.ito <m.ito@conol.jp>
2017-04-26 11:57:31 +0900

    action_mailer.default_url_optionsでHTTPSを指定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = ENV['HOST']
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   config.action_mailer.default_options = { from: ENV['FROM_ADDRESS'] }
   ActionMailer::Base.smtp_settings = {
     address: 'email-smtp.us-west-2.amazonaws.com',
