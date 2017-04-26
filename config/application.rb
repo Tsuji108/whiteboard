11151215d38ab101710e6503d01ae310818afb77
m.ito <m.ito@conol.jp>
2017-04-26 18:00:50 +0900

    config.time_zone = 'Tokyo'追加

--- config/application.rb
+++ config/application.rb
@@ -3,8 +3,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.time_zone = 'Tokyo'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -12,6 +10,7 @@ Bundler.require(*Rails.groups)
 module Whiteboard
   class Application < Rails::Application
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
