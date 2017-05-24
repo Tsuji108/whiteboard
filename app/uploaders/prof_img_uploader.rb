521351848e006ffca62afeb574c560cc12461560
m.ito <m.ito@conol.jp>
2017-05-24 11:09:12 +0900

    uploader修正

--- app/uploaders/prof_img_uploader.rb
+++ app/uploaders/prof_img_uploader.rb
@@ -1,7 +1,7 @@
 class ProfImgUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
-  process resize_to_fill: [100, 100, "Center"]
+  process resize_to_fill: [100, 100]
 
   if Rails.env.production?
     storage :fog
