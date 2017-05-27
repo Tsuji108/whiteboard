b31b0cdfb7426762089a060463e2b045ab1b789c
m.ito <m.ito@conol.jp>
2017-05-27 15:13:29 +0900

    フラッシュメッセージの高さ＆キャッシュ情報修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
+  before_action :set_cache_headers
+
   protect_from_forgery with: :exception
+
   include SessionsHelper
   include UsersHelper
   include MailingListsHelper
@@ -23,4 +26,13 @@ class ApplicationController < ActionController::Base
   def admin_user
     redirect_to root_url unless current_user.admin?
   end
+
+  private
+
+    # キャッシュを削除
+    def set_cache_headers
+      response.headers["Cache-Control"] = "no-cache, no-store"
+      response.headers["Pragma"] = "no-cache"
+      response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+    end
 end
